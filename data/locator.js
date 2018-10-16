5fa220105f2ff0bf059f8b784530d1be78a846cb
Artur Senko <asenko@apple.com>
2018-10-15 19:28:10 -0700

    fixed alignment

--- data/locator.js
+++ data/locator.js
@@ -1,191 +1,191 @@
 module.exports = {
 
-    client: {
-        header: '.custom-header',
-        app: '.container .p-5',
-        footer: '.custom-footer',
-        favicon: '#favicon'
-    },
+  client: {
+    header: '.custom-header',
+    app: '.container .p-5',
+    footer: '.custom-footer',
+    favicon: '#favicon'
+  },
 
-    globalHeader: {
-        header: '.custom-header',
-        headerText: '.custom-header span',
-        headerImage: '.custom-header span img'
-    },
+  globalHeader: {
+    header: '.custom-header',
+    headerText: '.custom-header span',
+    headerImage: '.custom-header span img'
+  },
 
-    loginPage: {
-        email: '#email',
-        password: '#pass',
-        loginButton: '#login',
-        errorMessage: '.alert-danger',
-        requiredText: '.text-sm-left',
-        registrationButton: '#registration'
-    },
+  loginPage: {
+    email: '#email',
+    password: '#pass',
+    loginButton: '#login',
+    errorMessage: '.alert-danger',
+    requiredText: '.text-sm-left',
+    registrationButton: '#registration'
+  },
 
-    registrationPage: {
-        firstName: '#fname',
-        lastName: '#lname',
-        errorMessage: '.alert-danger',
-        registerButton: '#registration',
-        email: '#email',
-        password: '#pass'
-    },
+  registrationPage: {
+    firstName: '#fname',
+    lastName: '#lname',
+    errorMessage: '.alert-danger',
+    registerButton: '#registration',
+    email: '#email',
+    password: '#pass'
+  },
 
-    newBugReportPage: {
-        summaryTxtFld: '.input-group.mb-2>input',
-        assigneeTxtFld: '.Dropdown-placeholder',
-        bugReportForm: '#bug-form',
-        summaryField: '#summary',
-        stepsToReproduce: '#str',
-        actualResult: '#actual',
-        expectedResult: '#expected',
-        attachmentLink: '#attachment',
-        assigneeDropDown: '#assignee div.Dropdown-root',
-        priorities: '#priority div.Dropdown-root',
-        severities: '#severity div.Dropdown-root',
-        assigneeDropDownPlaceHolder: '#assignee .Dropdown-placeholder',
-        priorityDropDownPlaceHolder: '#priority .Dropdown-placeholder',
-        severityDropDownPlaceHolder: '#severity .Dropdown-placeholder',
-        reproDropDownPlaceHolder: '#repro .Dropdown-placeholder',
-        versionDropDownPlaceHolder: '#version .Dropdown-placeholder',
-        requiredFieldText: '.text-sm-left'
-    },
+  newBugReportPage: {
+    summaryTxtFld: '.input-group.mb-2>input',
+    assigneeTxtFld: '.Dropdown-placeholder',
+    bugReportForm: '#bug-form',
+    summaryField: '#summary',
+    stepsToReproduce: '#str',
+    actualResult: '#actual',
+    expectedResult: '#expected',
+    attachmentLink: '#attachment',
+    assigneeDropDown: '#assignee div.Dropdown-root',
+    priorities: '#priority div.Dropdown-root',
+    severities: '#severity div.Dropdown-root',
+    assigneeDropDownPlaceHolder: '#assignee .Dropdown-placeholder',
+    priorityDropDownPlaceHolder: '#priority .Dropdown-placeholder',
+    severityDropDownPlaceHolder: '#severity .Dropdown-placeholder',
+    reproDropDownPlaceHolder: '#repro .Dropdown-placeholder',
+    versionDropDownPlaceHolder: '#version .Dropdown-placeholder',
+    requiredFieldText: '.text-sm-left'
+  },
 
-    navigationBar: {
-        newBugButton: '#new_bug',
-        allIssuesButton: '#all_issues',
-        logoutButton: '#logout',
-        navigetionBar: '[class="card-footer text-center mb-3"] .input-group-append',
-        cardFooter: '.card-footer',
-        submitButton: '#bug_add',
-        issuesTable: '.table'
-    },
+  navigationBar: {
+    newBugButton: '#new_bug',
+    allIssuesButton: '#all_issues',
+    logoutButton: '#logout',
+    navigetionBar: '[class="card-footer text-center mb-3"] .input-group-append',
+    cardFooter: '.card-footer',
+    submitButton: '#bug_add',
+    issuesTable: '.table'
+  },
 
-    globalFooter: {
-        footer: '.custom-footer',
-        footerText: 'body .custom-footer'
-    },
+  globalFooter: {
+    footer: '.custom-footer',
+    footerText: 'body .custom-footer'
+  },
 
-    bugReportFormDisabledState: {
-        bugTitle: '#bug',
-        bugReportDisbForm: '.badge',
-        attachment: '#attach_link',
-        textArea: '#str_value',
-        titleFont: '#summary_title',
-        valueFont: '#summary_value',
-        email: '#email',
-        password: '#pass',
-        loginButton: '#login',
-        allIssues: '#all_issues',
-        bugSummary: '#summary'
-    },
+  bugReportFormDisabledState: {
+    bugTitle: '#bug',
+    bugReportDisbForm: '.badge',
+    attachment: '#attach_link',
+    textArea: '#str_value',
+    titleFont: '#summary_title',
+    valueFont: '#summary_value',
+    email: '#email',
+    password: '#pass',
+    loginButton: '#login',
+    allIssues: '#all_issues',
+    bugSummary: '#summary'
+  },
 
-    commentsModule: {
-        input: '#summary',
-        inputActRes: '#actual',
-        allIssuesBtn: '#all_issues',
-        btn: '.btn-primary.btn-block',
-        btnAdd: '.btn.mt-2',
-        bugTitle: 'span.text-left=Register button has wrong text',
-        txtArea: '#str',
-        commentsTitle: '#comments_title',
-        registerButton: '#registration',
-        newCommentsTextArea: '#new_comment',
-        comments: '#comments',
-        noComments: '#no_comments',
-        bugTitleEmpty: 'span.text-left=Test. Empty comments bug report.'
-    },
+  commentsModule: {
+    input: '#summary',
+    inputActRes: '#actual',
+    allIssuesBtn: '#all_issues',
+    btn: '.btn-primary.btn-block',
+    btnAdd: '.btn.mt-2',
+    bugTitle: 'span.text-left=Register button has wrong text',
+    txtArea: '#str',
+    commentsTitle: '#comments_title',
+    registerButton: '#registration',
+    newCommentsTextArea: '#new_comment',
+    comments: '#comments',
+    noComments: '#no_comments',
+    bugTitleEmpty: 'span.text-left=Test. Empty comments bug report.'
+  },
 
-    bugList: {
-        bodyBugList: '#bug_list_page',
-        headerTable: 'thead',
-        bodyTable: 'tbody tr',
-        bugSummaryTitle: '#bug_summary',
-        bugPriorityTitle: '#bug_priority',
-        bugStatusTitle: '#bug_status',
-        bugReporterTitle: '#bug_reporter',
-        titleTable: '.table thead p',
-        fontfamilyTitle: '.font-segoe-ui',
-        textSummary: '.btn-link',
-        textPriority: 'tbody #priority',
-        textStatus: 'tbody #status',
-        textReporter: 'tbody #reporter'
+  bugList: {
+    bodyBugList: '#bug_list_page',
+    headerTable: 'thead',
+    bodyTable: 'tbody tr',
+    bugSummaryTitle: '#bug_summary',
+    bugPriorityTitle: '#bug_priority',
+    bugStatusTitle: '#bug_status',
+    bugReporterTitle: '#bug_reporter',
+    titleTable: '.table thead p',
+    fontfamilyTitle: '.font-segoe-ui',
+    textSummary: '.btn-link',
+    textPriority: 'tbody #priority',
+    textStatus: 'tbody #status',
+    textReporter: 'tbody #reporter'
 
-    },
+  },
 
-    bugReportEditMode: {
-        input: '#summary',
-        inputActRes: '#actual',
-        inputExpRes: '#expected',
-        inputAtach: '#attachment',
-        btnAllIssues: '#all_issues',
-        btnEdit: '#edit',
-        bugTitle: '#bug',
-        txtArea: '#str',
-        reporter: '.text-gray'
-    },
+  bugReportEditMode: {
+    input: '#summary',
+    inputActRes: '#actual',
+    inputExpRes: '#expected',
+    inputAtach: '#attachment',
+    btnAllIssues: '#all_issues',
+    btnEdit: '#edit',
+    bugTitle: '#bug',
+    txtArea: '#str',
+    reporter: '.text-gray'
+  },
 
-    loginPage: {
-        email: '#email',
-        password: '#pass',
-        loginButton: '#login',
-        errorMessage: '.alert-danger',
-        registrationButton: '#registration'
-    },
+  loginPage: {
+    email: '#email',
+    password: '#pass',
+    loginButton: '#login',
+    errorMessage: '.alert-danger',
+    registrationButton: '#registration'
+  },
 
-    registrationPage: {
-        firstName: '#fname',
-        lastName: '#lname',
-        errorMessage: '.alert-danger',
-        registerButton: '#registration'
-    },
+  registrationPage: {
+    firstName: '#fname',
+    lastName: '#lname',
+    errorMessage: '.alert-danger',
+    registerButton: '#registration'
+  },
 
-    globalFooter: {
-        footer: '.custom-footer',
-        footerText: 'body .custom-footer',
-    },
+  globalFooter: {
+    footer: '.custom-footer',
+    footerText: 'body .custom-footer',
+  },
 
-    bugReportTitles: {
-        summary: '#summary_title',
-        stepsToRep: '#str_title',
-        actRes: '#actual_title',
-        expRes: '#expected=title',
-        attach: '#attach_title',
-        assignee: '#assignee_title',
-        reporter: '#reporter_title',
-        version: '#version_title',
-        priority: '#priority_title',
-        severity: '#severity_value',
-        repro: '#repro_title',
-        comments: '#comments_title',
-        allIssuesBtn: '#all_issues',
-        btn: '#edit',
-        bugTitle: '#summary',
-        txtArea: '#str',
-        registerButton: '#registration'
-    },
+  bugReportTitles: {
+    summary: '#summary_title',
+    stepsToRep: '#str_title',
+    actRes: '#actual_title',
+    expRes: '#expected=title',
+    attach: '#attach_title',
+    assignee: '#assignee_title',
+    reporter: '#reporter_title',
+    version: '#version_title',
+    priority: '#priority_title',
+    severity: '#severity_value',
+    repro: '#repro_title',
+    comments: '#comments_title',
+    allIssuesBtn: '#all_issues',
+    btn: '#edit',
+    bugTitle: '#summary',
+    txtArea: '#str',
+    registerButton: '#registration'
+  },
 
-    logout: {
-        loginButton: '#login',
-        bugReportForm: '#bug-form',
-        logoutButton: '#logout',
-    },
+  logout: {
+    loginButton: '#login',
+    bugReportForm: '#bug-form',
+    logoutButton: '#logout',
+  },
 
-    controlBarFunctionality: {
-        bugReportFormDisabled: '#bug_report_disabled',
-        bugReportFormEdit: '#edit-mode-form',
-        controlBar: '#edit-mode-form',
-        cancelBtnDis: '#cancel',
-        cancelBtnEdit: '#cancel_edit',
-        editBtn: '#edit',
-        submitBtn: '#submit',
-        anyInput: 'input',
-        anyDropDown: '#edit-mode-form .Dropdown-control',
-        anyDropInControlBar: 'tbody .Dropdown-control',
-        textAreaDis: '#str_value',
-        textAreaEdt: '#str',
-        bugSummary: '#summary',
-        badgeDropDownEdt: '#edit-mode-form tbody .Dropdown-control'
-    }
+  controlBarFunctionality: {
+    bugReportFormDisabled: '#bug_report_disabled',
+    bugReportFormEdit: '#edit-mode-form',
+    controlBar: '#edit-mode-form',
+    cancelBtnDis: '#cancel',
+    cancelBtnEdit: '#cancel_edit',
+    editBtn: '#edit',
+    submitBtn: '#submit',
+    anyInput: 'input',
+    anyDropDown: '#edit-mode-form .Dropdown-control',
+    anyDropInControlBar: 'tbody .Dropdown-control',
+    textAreaDis: '#str_value',
+    textAreaEdt: '#str',
+    bugSummary: '#summary',
+    badgeDropDownEdt: '#edit-mode-form tbody .Dropdown-control'
+  }
 
 };
--- data/test.data.js
+++ data/test.data.js
@@ -1,220 +1,220 @@
 module.exports = {
 
-    client: {
-        pageTitle: "Bug Tracker",
-        faviconLink: 'https://reactbugtracker.com/favicon.ico'
-    },
+  client: {
+    pageTitle: "Bug Tracker",
+    faviconLink: 'https://reactbugtracker.com/favicon.ico'
+  },
 
-    globalHeader: {
-        headerTitle: "BugTracker",
-        headerColor: "rgba(0,0,0,0.125)",
-        headerFontSize: "30px",
-        fontWeight: "400",
-        fontFamily: "segoe ui",
-        fontColor: "#212529",
-        imageSize: "30x30",
-        textAlign: "center"
-    },
+  globalHeader: {
+    headerTitle: "BugTracker",
+    headerColor: "rgba(0,0,0,0.125)",
+    headerFontSize: "30px",
+    fontWeight: "400",
+    fontFamily: "segoe ui",
+    fontColor: "#212529",
+    imageSize: "30x30",
+    textAlign: "center"
+  },
 
-    loginPage: {
-        emailPlaceholderText: "Email *",
-        emailFieldFontSize: "16px",
-        emailFieldFontWeight: "400",
-        emailFieldFontFamily: "segoe ui",
-        emailFieldFontColor: "#495057",
-        emailFieldFocusColor: "#80bdff",
+  loginPage: {
+    emailPlaceholderText: "Email *",
+    emailFieldFontSize: "16px",
+    emailFieldFontWeight: "400",
+    emailFieldFontFamily: "segoe ui",
+    emailFieldFontColor: "#495057",
+    emailFieldFocusColor: "#80bdff",
 
-        loginButtonFontSize: "16px",
-        loginButtonFontWeight: "400",
-        loginButtonFontFamily: "segoe ui",
-        loginButtonFontColor: "#ffffff",
-        loginButtonColor: "#17a2b8",
+    loginButtonFontSize: "16px",
+    loginButtonFontWeight: "400",
+    loginButtonFontFamily: "segoe ui",
+    loginButtonFontColor: "#ffffff",
+    loginButtonColor: "#17a2b8",
 
-        loginButtonFocusColor: "#138496" //hover background-color
+    loginButtonFocusColor: "#138496" //hover background-color
 
-        //Verify that login button with text “Login”, middle-left aligned
+    //Verify that login button with text “Login”, middle-left aligned
 
 
-    },
+  },
 
-    user: {
-        email: 'tester@test.com',
-        password: 'test'
-    },
+  user: {
+    email: 'tester@test.com',
+    password: 'test'
+  },
 
-    loginPageFunctionality: {
-        minLength: 1,
-        midLength: 22,
-        maxLength: 45,
-        noLoginOrPassError: 'Specify email and password',
-        wrongLoginError: 'User with this email does not exist',
-        wrongPasswordError: 'Password is incorrect',
-        oneChar: 'A',
-        errorDesignBackgroundColor: 'rgba(248,215,218,1)',
-        errorDesignBorderColor: 'rgb(245,198,203)',
-        errorDesignFontFamily: 'segoe ui',
-        errorDesignFontSize: '16px',
-        errorDesignFontWeight: '400',
-        errorDesignTextAlign: 'center',
-        errorDesignFontColor: 'rgba(114,28,36,1)'
-    },
+  loginPageFunctionality: {
+    minLength: 1,
+    midLength: 22,
+    maxLength: 45,
+    noLoginOrPassError: 'Specify email and password',
+    wrongLoginError: 'User with this email does not exist',
+    wrongPasswordError: 'Password is incorrect',
+    oneChar: 'A',
+    errorDesignBackgroundColor: 'rgba(248,215,218,1)',
+    errorDesignBorderColor: 'rgb(245,198,203)',
+    errorDesignFontFamily: 'segoe ui',
+    errorDesignFontSize: '16px',
+    errorDesignFontWeight: '400',
+    errorDesignTextAlign: 'center',
+    errorDesignFontColor: 'rgba(114,28,36,1)'
+  },
 
-    bugReportFormDisabledState: {
-        attachment: 'a',
-        fontWeightAttach: "400",
-        fontFamilyAttach: "Segoe UI",
-        fontSizeAttach: "16px",
-        minRowReq: 2,
-        backColorSteps: 'rgba(233,236,239,0.2)',
-        borderColor: 'rgba(0,0,0,0.5)',
-        fontWeight: "700",
-        fontFamily: "segoe ui",
-        fontSize: "16px",
-        fontColor: "rgba(33,37,41,1)",
-        fontWeightValue: "400",
-        fontFamilyValue: "segoe ui",
-        fontSizeValue: "16px",
-        fontColorValue: "rgba(0,0,0,0.49)"
-    },
+  bugReportFormDisabledState: {
+    attachment: 'a',
+    fontWeightAttach: "400",
+    fontFamilyAttach: "Segoe UI",
+    fontSizeAttach: "16px",
+    minRowReq: 2,
+    backColorSteps: 'rgba(233,236,239,0.2)',
+    borderColor: 'rgba(0,0,0,0.5)',
+    fontWeight: "700",
+    fontFamily: "segoe ui",
+    fontSize: "16px",
+    fontColor: "rgba(33,37,41,1)",
+    fontWeightValue: "400",
+    fontFamilyValue: "segoe ui",
+    fontSizeValue: "16px",
+    fontColorValue: "rgba(0,0,0,0.49)"
+  },
 
-    bugReportEditMode: {
-        tagInput: 'input',
-        fontSize: '16px',
-        fontColor: 'rgba(73,80,87,1)',
-        textAlign: 'start',
-        fontFamily: 'segoe ui',
-        fontWeight: '400',
-        tagTextArea: 'textarea',
-        tagSpan: 'span',
-        summaryArray: [
-            'Summary:',
-            'Steps to Reproduce:',
-            'Actual Result:',
-            'Expected result:',
-            'Attachment:',
-            'Assignee:',
-            'Reporter:',
-            'Version:',
-            'Priority:',
-            'Severity:',
-            'Repro:',
-            'Comments:'
-        ]
-    },
+  bugReportEditMode: {
+    tagInput: 'input',
+    fontSize: '16px',
+    fontColor: 'rgba(73,80,87,1)',
+    textAlign: 'start',
+    fontFamily: 'segoe ui',
+    fontWeight: '400',
+    tagTextArea: 'textarea',
+    tagSpan: 'span',
+    summaryArray: [
+      'Summary:',
+      'Steps to Reproduce:',
+      'Actual Result:',
+      'Expected result:',
+      'Attachment:',
+      'Assignee:',
+      'Reporter:',
+      'Version:',
+      'Priority:',
+      'Severity:',
+      'Repro:',
+      'Comments:'
+    ]
+  },
 
-    registrationPageFunctionality: {
-        firstNameMinLength: 1,
-        firstNameMaxLength: 20,
-        lastNameMinLength: 1,
-        lastNameMaxLength: 20,
-        emptyFieldError: 'Please fill all the fields',
-        emailFieldMinLength: 1,
-        emailFieldMiddleLength: 25,
-        emailFieldMaxLength: 45,
-        emailAlreadyRegisteredError: 'User with this email already exists',
-        passwordFieldMinLength: 1,
-        passwordFieldMaxLength: 45,
-        passwordFieldMiddleLength: 20,
-        errorBackgrColor: 'rgba(248,215,218,1)',
-        errorBorderColor: 'rgba(245,198,203,1)',
-        errorFontFamily: 'segoe ui',
-        errorFontSize: '16px',
-        errorFontWeight: '400',
-        errorFontColor: 'rgba(114,28,36,1)'
-    },
+  registrationPageFunctionality: {
+    firstNameMinLength: 1,
+    firstNameMaxLength: 20,
+    lastNameMinLength: 1,
+    lastNameMaxLength: 20,
+    emptyFieldError: 'Please fill all the fields',
+    emailFieldMinLength: 1,
+    emailFieldMiddleLength: 25,
+    emailFieldMaxLength: 45,
+    emailAlreadyRegisteredError: 'User with this email already exists',
+    passwordFieldMinLength: 1,
+    passwordFieldMaxLength: 45,
+    passwordFieldMiddleLength: 20,
+    errorBackgrColor: 'rgba(248,215,218,1)',
+    errorBorderColor: 'rgba(245,198,203,1)',
+    errorFontFamily: 'segoe ui',
+    errorFontSize: '16px',
+    errorFontWeight: '400',
+    errorFontColor: 'rgba(114,28,36,1)'
+  },
 
-    navigationBarFunctionality: {
-        length: "flex",
-        navBarColor: "rgba(0,0,0,0.03)",
-        navBarFont: "segoe ui",
-        navBarFontSize: "16px",
-        navBarFontWeight: "400",
-        navBarButtonsFontColor: "rgba(255,255,255,1)",
-        BackgroundColor1: "rgba(23,162,184,1)",
-        BackgroundColor2: "rgba(108,117,125,1)",
-        HoverBackgroundColor1: "rgba(19,132,150,1)",
-        HoverBackgroundColor2: "rgba(90,98,104,1)"
-    },
+  navigationBarFunctionality: {
+    length: "flex",
+    navBarColor: "rgba(0,0,0,0.03)",
+    navBarFont: "segoe ui",
+    navBarFontSize: "16px",
+    navBarFontWeight: "400",
+    navBarButtonsFontColor: "rgba(255,255,255,1)",
+    BackgroundColor1: "rgba(23,162,184,1)",
+    BackgroundColor2: "rgba(108,117,125,1)",
+    HoverBackgroundColor1: "rgba(19,132,150,1)",
+    HoverBackgroundColor2: "rgba(90,98,104,1)"
+  },
 
-    globalFooter: {
-        footerTitle: new Date().getFullYear().toString() + " Bug Tracker by React Web Development",
-        footerColor: "rgba(0,0,0,0.125)",
-        footerFontSize: "16px",
-        fontWeight: "400",
-        fontFamily: "segoe ui",
-        fontColor: "#212529"
-    },
+  globalFooter: {
+    footerTitle: new Date().getFullYear().toString() + " Bug Tracker by React Web Development",
+    footerColor: "rgba(0,0,0,0.125)",
+    footerFontSize: "16px",
+    fontWeight: "400",
+    fontFamily: "segoe ui",
+    fontColor: "#212529"
+  },
 
-    newBugReportPage: {
-        fontFamily: 'segoe ui',
-        fontSize: '16px',
-        fontWeight: '400',
-        fontColor: 'rgba(73,80,87,1)',
-        textAlignStart: 'start',
-        textAlignLeft: 'left',
-        fontColorRf: 'rgba(33,37,41,1)',
-        textAlign: 'start',
-        textAlign1: 'left',
-        placeholder: ['Summary *', 'Steps to reproduce *', 'Actual result *', 'Expected result *', 'Attachment link',
-            'Assignee *', 'Priority *', 'Severity *', 'Repro *', 'Version']
-    },
+  newBugReportPage: {
+    fontFamily: 'segoe ui',
+    fontSize: '16px',
+    fontWeight: '400',
+    fontColor: 'rgba(73,80,87,1)',
+    textAlignStart: 'start',
+    textAlignLeft: 'left',
+    fontColorRf: 'rgba(33,37,41,1)',
+    textAlign: 'start',
+    textAlign1: 'left',
+    placeholder: ['Summary *', 'Steps to reproduce *', 'Actual result *', 'Expected result *', 'Attachment link',
+      'Assignee *', 'Priority *', 'Severity *', 'Repro *', 'Version']
+  },
 
-    bugReportPageControlBar: {
-        btnFontFamily: 'segoe ui',
-        btnFontSize: '16px',
-        btnFontWeight: '400',
-        btnFontColor: '#ffffff',
-        btnTextAlign: 'center',
+  bugReportPageControlBar: {
+    btnFontFamily: 'segoe ui',
+    btnFontSize: '16px',
+    btnFontWeight: '400',
+    btnFontColor: '#ffffff',
+    btnTextAlign: 'center',
 
-        cancelBtnBackgroundColor: '#17a2b8',
-        cancelBtnHoverBackgroundColor: '#138496',
-    },
+    cancelBtnBackgroundColor: '#17a2b8',
+    cancelBtnHoverBackgroundColor: '#138496',
+  },
 
-    bugList: {
-        bugSummaryTitle: 'Bug Summary',
-        bugPriorityTitle: 'Priority',
-        bugStatusTitle: 'Status',
-        bugReporterTitle: 'Reporter',
-        textAlignHeader: 'center',
-        fontSizeHeader: '16px',
-        fontWeightHeader: '700',
-        fontFamilyHeader: 'segoe ui',
-        fontColorHeader: '#212529',
-        textAlignSummary: 'left',
-        fontColorSummary: '#007bff',
-        fontWeightSummary: '400',
-        fontFamilySummary: 'segoe ui',
-        fontSizeSummary: '16px',
-        textAlignPriority: 'center',
-        fontColorPriority: '#212529',
-        fontWeightPriority: '400',
-        fontFamilyPriority: 'segoe ui',
-        fontSizePriority: '16px',
-        textAlignStatus: 'center',
-        fontColorStatus: '#212529',
-        fontWeightStatus: '400',
-        fontFamilyStatus: 'segoe ui',
-        fontSizeStatus: '16px',
-        textAlignReporter: 'center',
-        fontColorReporter: '#212529',
-        fontWeightReporter: '400',
-        fontFamilyReporter: 'segoe ui',
-        fontSizeReporter: '16px'
-    },
+  bugList: {
+    bugSummaryTitle: 'Bug Summary',
+    bugPriorityTitle: 'Priority',
+    bugStatusTitle: 'Status',
+    bugReporterTitle: 'Reporter',
+    textAlignHeader: 'center',
+    fontSizeHeader: '16px',
+    fontWeightHeader: '700',
+    fontFamilyHeader: 'segoe ui',
+    fontColorHeader: '#212529',
+    textAlignSummary: 'left',
+    fontColorSummary: '#007bff',
+    fontWeightSummary: '400',
+    fontFamilySummary: 'segoe ui',
+    fontSizeSummary: '16px',
+    textAlignPriority: 'center',
+    fontColorPriority: '#212529',
+    fontWeightPriority: '400',
+    fontFamilyPriority: 'segoe ui',
+    fontSizePriority: '16px',
+    textAlignStatus: 'center',
+    fontColorStatus: '#212529',
+    fontWeightStatus: '400',
+    fontFamilyStatus: 'segoe ui',
+    fontSizeStatus: '16px',
+    textAlignReporter: 'center',
+    fontColorReporter: '#212529',
+    fontWeightReporter: '400',
+    fontFamilyReporter: 'segoe ui',
+    fontSizeReporter: '16px'
+  },
 
-    commentsModule: {
-        fontFamily: 'segoe ui',
-        fontSize: '16px',
-        fontWeight: '400',
-        titleFontWeight: '700',
-        fontColor: '#212529',
-        textAlign: 'left',
-        commTitle: 'Comments:',
-        noCommentsTitle: 'No comments'
-    },
+  commentsModule: {
+    fontFamily: 'segoe ui',
+    fontSize: '16px',
+    fontWeight: '400',
+    titleFontWeight: '700',
+    fontColor: '#212529',
+    textAlign: 'left',
+    commTitle: 'Comments:',
+    noCommentsTitle: 'No comments'
+  },
 
-    controlBarFunctionality: {
-        editButtonName: 'Edit',
-        submitButtonName: 'Submit'
-    }
+  controlBarFunctionality: {
+    editButtonName: 'Edit',
+    submitButtonName: 'Submit'
+  }
 };
